e05bc01297d22f0b691bd8ccdd2d5aa00dd4a10b
SUDHANSHU DUBEY <manudubey249@gmail.com>
2026-01-27 11:21:02 +0530

    added users

--- api/axios.js
+++ api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    //baseURL: "http://localhost:5000/api",
+    baseURL: "https://ml-backend-93ug.onrender.com/api",
     headers: {
         "Content-Type": "application/json",
     },
