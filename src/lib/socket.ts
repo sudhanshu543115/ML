02fe23197da940173c9b7df2767981e9234d3c0c
SUDHANSHU DUBEY <manudubey249@gmail.com>
2026-01-28 14:28:21 +0530

    Update socket.ts

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://ml-backend-93ug.onrender.com/api", {
   withCredentials: true,
   autoConnect: true,
 });
